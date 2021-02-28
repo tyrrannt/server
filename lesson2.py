a991991841d86a90aada5082316d162e52eaf69e
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-02-28 21:59:35 +0300

    решено 3 задания

--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,9 @@
 import json
 import re
+import yaml
 
+"""Задание на закрепление знаний по модулю CSV. Написать скрипт, осуществляющий выборку определенных данных из файлов 
+info_1.txt, info_2.txt, info_3.txt и формирующий новый «отчетный» файл в формате CSV. """
 where_looking = ('info_1.txt', 'info_2.txt', 'info_3.txt')
 
 
@@ -41,6 +44,12 @@ def get_data(where, *args):
     return main_data, os_prod_list, os_name_list, os_code_list, os_type_list
 
 
+write_to_csv('main_data.csv')
+
+"""2. Задание на закрепление знаний по модулю json. Есть файл orders в формате JSON с информацией о заказах. Написать 
+скрипт, автоматизирующий его заполнение данными. """
+
+
 def write_order_to_json(item, quantity, price, buyer, date):
     # {"orders": []}
     with open('orders.json') as FD:
@@ -51,8 +60,32 @@ def write_order_to_json(item, quantity, price, buyer, date):
         FD.write(json.dumps(content, indent=4))
 
 
-# write_to_csv('main_data.csv')
-# write_order_to_json('Mouse', '5', '1500', 'Alex Byuer', '23.02.2021')
-# write_order_to_json('Notebook', '1', '15000', 'Mr. Black', '03.02.2021')
-# write_order_to_json('Lamp', '10', '10', 'Bob Charley', '20.01.2021')
-# write_order_to_json('Red Rose', '500', '35', 'Angelica Smith', '11.01.2021')
+write_order_to_json('Mouse', '5', '1500', 'Alex Byuer', '23.02.2021')
+write_order_to_json('Notebook', '1', '15000', 'Mr. Black', '03.02.2021')
+write_order_to_json('Lamp', '10', '10', 'Bob Charley', '20.01.2021')
+write_order_to_json('Red Rose', '500', '35', 'Angelica Smith', '11.01.2021')
+
+"""
+Задание на закрепление знаний по модулю yaml. Написать скрипт, автоматизирующий сохранение данных в файле YAML-формата.
+"""
+network = {
+    "ethernets":
+        {"enp0s3":
+             {"dhcp4": "true"},
+         "enp0s8":
+             {"dhcp4": "true"}
+         },
+    "version": "2",
+    "access-points":
+        {
+            "Super™€":
+                {"password": "12345678"}
+        }
+}
+
+with open('file.yaml', 'w') as f_n:
+    yaml.dump(network, f_n, default_flow_style=False, allow_unicode=True)
+
+with open('file.yaml') as f_n:
+    print(f_n.read())
+# Результат работы совпадает
